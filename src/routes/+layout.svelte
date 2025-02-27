5a7b6e53422649c92d4f6654ad8ac93b0a5278ab
Nathan Neri <nahtann@outlook.com>
2025-02-26 22:31:51 -0300

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<main>
+	<div class="container mx-auto px-4">
+		{@render children()}
+	</div>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { DotLottieSvelte } from '@lottiefiles/dotlottie-svelte';
+</script>
+
+<h1 class="mt-10 mb-8 text-center text-4xl font-bold md:text-5xl lg:text-6xl">
+	O começo de uma nova história
+</h1>
+
+<!-- Lottie animation container -->
+<!--<div bind:this={lootieContainer} class="mx-auto h-64 w-full max-w-md md:h-80"></div>-->
+<DotLottieSvelte src="src/lib/animations/baby.lottie" loop autoplay />
